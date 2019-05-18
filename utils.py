6d5f2af6b5ce8ae52500ef14d9735d348e737cc8
DeepLearning VM <bowen-wu@hotmail.com>
2019-05-18 22:17:01 +0000

    Add conversion from RGB to YIQ/YUV

--- utils.py
+++ utils.py
@@ -48,16 +48,44 @@ def un_normalize(tensor, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
     #print(np.min(image))
     return image
 
-def validate(image, stye_transfer_net, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+def validate(image, style_transfer_net, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], device='cpu'):
     transform = transforms.Compose([transforms.Resize(512),transforms.ToTensor(),transforms.Normalize(mean, std)])
     
-    image_tensor = transform(image)
-    styled_image = stye_transfer_net(image_tensor.unsqueeze(0))
-    styled_image = un_normalize(styled_image)
+    style_transfer_net.to(device)
+    image_tensor = transform(image).to(device)
+    styled_image = style_transfer_net(image_tensor.unsqueeze(0))
+    styled_image = un_normalize(styled_image.to('cpu').detach())
     
-    plt.imshow(styled_image[0])
+    fig, axes = plt.subplots(1,2,figsize=(30,30))
+    axes[0].imshow(image)
+    axes[1].imshow(styled_image[0])
     plt.show()
     
+def rgb_to_ycbcr(images, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], device='cuda'):
+    """
+    ref: https://discuss.pytorch.org/t/how-to-change-a-batch-rgb-images-to-ycbcr-images-during-training/3799 by smth
+    """
+    mean = torch.tensor(mean, device=device).view(1,3,1,1)
+    std = torch.tensor(std, device=device).view(1,3,1,1)
+    
+    images = images*std + mean
+    
+    KR = 0.299
+    KG = 0.587
+    KB = 0.114
+    
+    # input is mini-batch N x 3 x H x W of an RGB image
+    output = images.new(*images.size())
+    
+    # similarly write output[:, 1, :, :] and output[:, 2, :, :] using formulas from https://en.wikipedia.org/wiki/YCbCr
+    output[:, 0, :, :] = images[:, 0, :, :] * KR + images[:, 1, :, :] * KG + images[:, 2, :, :] * KB #Y
+    #output[:, 1, :, :] = images[:, 0, :, :] * -0.14713 + images[:, 1, :, :] * 0.28886+ images[:, 2, :, :] * 0.436 #Cb
+    #output[:, 2, :, :] = images[:, 0, :, :] * 0.615 + images[:, 1, :, :] * -0.51499 + images[:, 2, :, :] * -0.10001 #Cr
+    
+    # ref: https://www.eembc.org/techlit/datasheets/yiq_consumer.pdf, 
+    # https://blogs.mathworks.com/cleve/2016/12/12/my-first-matrix-rgb-yiq-and-color-cubes/
+    output[:, 1, :, :] = images[:, 0, :, :] * 0.5959 - images[:, 1, :, :] * 0.2744 - images[:, 2, :, :] * 0.3216 #I
+    output[:, 2, :, :] = images[:, 0, :, :] * 0.2115 - images[:, 1, :, :] * -0.5229 + images[:, 2, :, :] * 0.3114 #Q
     
     
-    
+    return output
